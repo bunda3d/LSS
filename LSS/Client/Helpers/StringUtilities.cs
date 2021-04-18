807d7c4be4c57fec7e47a558325846c78616eb98
bunda3d <bunda3@yahoo.com>
2021-04-18 06:52:44 -0500

    Initial commit

---
+++ LSS/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSS.Client.Helpers
+{
+	public class StringUtilities
+	{
+		public static string toUpperCase(string value) => value.ToUpper();
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LSS
+ Business Modernization App
