45c9470c31b34a2914b288a6f4b1b17b88b07b76
bunda3d <bunda3@yahoo.com>
2021-05-14 22:03:27 -0500

    added some product pages

--- LSS/Client/Helpers/RepositoryInMemory.cs
+++ LSS/Client/Helpers/RepositoryInMemory.cs
@@ -12,9 +12,30 @@ namespace LSS.Client.Helpers
 		{
 			return new List<Suit>()
 			{
-				new Suit(){Title = "Spider-Man: Far From Home", ReleaseDate = new DateTime(2019, 7, 2) },
-				new Suit(){Title = "Moana", ReleaseDate = new DateTime(2016, 11, 23) },
-				new Suit(){Title = "Inception", ReleaseDate = new DateTime(2010, 7, 16) }
+				new Suit(){
+					Title = "Mens Grey Suit: 3-button ensemble",
+					ReleaseDate = new DateTime(2019, 7, 2),
+					Price = 700,
+					Poster = "/img/product/suit01.jpg"
+				},
+				new Suit(){
+					Title = "Extra Slim Light Gray Plaid Linen Blend Suit",
+					ReleaseDate = new DateTime(2016, 11, 23),
+					Price = 500,
+					Poster = "/img/product/suit03a.png"
+				},
+				new Suit(){
+					Title = "Grey Check Suit Jacket and Pant combo",
+					ReleaseDate = new DateTime(2010, 7, 16),
+					Price = 600,
+					Poster = "/img/product/suit02.jpg"
+				},
+				new Suit(){
+					Title = "Rust Chambray Solid Style Jacket and Pant",
+					ReleaseDate = new DateTime(2010, 7, 16),
+					Price = 400,
+					Poster = "/img/product/suit04a.png"
+				}
 			};
 		}
 	}
--- LSS/Shared/Entities/Suit.cs
+++ LSS/Shared/Entities/Suit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,33 @@ namespace LSS.Shared.Entities
 	public class Suit
 	{
 		public string Title { get; set; }
+
 		public DateTime ReleaseDate { get; set; }
+
+		public string Poster { get; set; }
+
+		[DataType(DataType.Currency)]
+		[DisplayFormat(ApplyFormatInEditMode =true, DataFormatString = "{0.c}")]
+		public decimal Price { get; set; }
+
+		public string TitleBrief
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(Title))
+				{
+					return null;
+				}
+
+				if (Title.Length > 60)
+				{
+					return Title.Substring(0, 60) + "...";
+				}
+				else
+				{
+					return Title;
+				}
+			}
+		}
 	}
 }
