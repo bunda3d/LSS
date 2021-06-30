a6448e393e7ceb023b97bcf7c771de07615fa23a
bunda3d <bunda3@yahoo.com>
2021-06-30 00:55:19 -0500

    update forms layout for Identity login and register

--- LSS/Client/Helpers/IHttpService.cs
+++ LSS/Client/Helpers/IHttpService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace LSS.Client.Helpers
 {
